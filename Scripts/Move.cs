ffe3ae788891e78c952d7df2faac9b08e9df91ee
Johnathan-Eshel <jonathan.eshel1@gmail.com>
2018-01-22 15:54:59 +0200

    Add files via upload

---
+++ Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float walkSpeed = 10.0f;
+    public bool facingRight = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if (move < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (move > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
+}
