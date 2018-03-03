c3af9506b0609eae206054637fe749793861ea46
Johnathan-Eshel <jonathan.eshel1@gmail.com>
2018-03-03 17:51:41 +0200

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float walkSpeed = 10.0f;
+    public bool facingRight = true;
+    public double Xspeed;
+    public double Yspeed;
+    // Use this for initialization
+    void Start()
+    {
+        Xspeed = 2;
+        Yspeed = 1.5;
+    }
+  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if()
+        /*float move = Input.GetAxis("Horizontal");
+        if (move < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (move > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }*/
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
+}
---
+++ shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject projectileprefab; 
+
+    private List<GameObject> Projectiles = new List<GameObject>();
+
+    private float projectilevelocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+        projectilevelocity = 2;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject bullet = (GameObject)Instantiate(projectileprefab, transform.position, Quaternion.identity);
+            Projectiles.Add(bullet);
+        }
+
+        for(int i = 0; i < Projectiles.Count; i++)
+        {
+            GameObject goBullet = Projectiles[i];
+            if (goBullet != null)
+                goBullet.transform.Translate(new Vector3(1, 0) * Time.deltaTime  * projectilevelocity);
+
+        }
+	}
+}
